066798a8f1e886d623cd2f48801692641288be44
InduRenazent <indu@renazentit.com>
2026-08-28 18:43:09 +0530

    force loging passed

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -113,7 +113,7 @@ export class AuthService {
       ComputerUser: 'User1',
       InternetIP: '192.158.1.38',
       SystemTimeUTC: currentUTCDateTime,
-      ForceLogin: true,
+      ForceLogin: forcelogin,
     };
 
     return this.http.post<any>(API_URL, ReqBody);
